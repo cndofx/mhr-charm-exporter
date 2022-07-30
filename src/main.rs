4e8fb35d533c6cc29ae25a7362d8604db9e274c1
Trevor Tilby <trevor.tilby@gmail.com>
2022-07-30 16:16:59 -0600

    use anyhow instead of .unwrap() in main function
    users will see more friendly error messages this way

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,21 @@ use std::fs::File;
 use std::io::{BufWriter, Write};
 
 #[cfg(windows)]
-fn main() {
-    let proc = process::get_process_handle("MonsterHunterRise.exe").unwrap();
+fn main() -> anyhow::Result<()> {
+    let proc = process::get_process_handle("MonsterHunterRise.exe")?;
 
     let manager = DataManager::new(proc);
 
     let charms = manager.get_all_charms();
 
-    let file = File::create("charms.txt").unwrap();
+    let file = File::create("charms.txt")?;
     let mut writer = BufWriter::new(&file);
 
     for c in charms {
-        writeln!(&mut writer, "{}", c).unwrap();
+        writeln!(&mut writer, "{}", c)?;
         println!("{}", c);
     }
 
     println!("\ncharms saved to charms.txt");
+    Ok(())
 }
